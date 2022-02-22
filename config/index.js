8fc02545797fd4d6cd6a28c0e0b64d18f34c3577
Fures Labs <gmonksnft@gmail.com>
2022-02-22 13:11:59 +0700

    update

--- config/index.js
+++ config/index.js
@@ -1,9 +1,9 @@
 export const config = {
   env: "prod",
   LOCAL_API_URL: "localhost:3000",
-  API_URL: "odp-rarity.vercel.app",
-  COLLECTION_NAME: "OneDayPunks",
-  COLLECTION_TITLE: "One Day Punks",
-  COLLECTION_DESCRIPTION: '10k "One Day I\'ll Be A Punk"-punks.',
-  COLLECTION_IMG_LINK: "https://punkscape.xyz/og.png",
+  API_URL: "62147d1f4df7e54209083c9a--elated-wescoff-9a2559.netlify.app",
+  COLLECTION_NAME: "GMONKS",
+  COLLECTION_TITLE: "GMONKS",
+  COLLECTION_DESCRIPTION: 'GMONKS An NFT project from Indonesia, a collection of 888 MONKS on the Polygon blockchain. GMONKS complies with the ERC721 standard, so they are all as unique as we are.',
+  COLLECTION_IMG_LINK: "https://pbs.twimg.com/profile_banners/1487793741830057990/1645436831/1500x500",
 };
